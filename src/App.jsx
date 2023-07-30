12a67b4a34d57fceb3dcc96fbce75c93f5dfbd74
arvindh-ar <88005455+arvindh-ar@users.noreply.github.com>
2023-07-30 21:17:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import NavBar from './components/navbar.jsx';
+import NavBar from './components/navbar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css'
 
