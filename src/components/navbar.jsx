da2f53743469b1c135e11399e673292d142eb297
arvindh-ar <88005455+arvindh-ar@users.noreply.github.com>
2023-07-31 01:25:50 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import About from '../pages/about'
+import Home from '../pages/home'
 import {
   Nav,
   NavLink,
@@ -9,37 +12,48 @@ import {
 } from './NavbarElements';
   
 const Navbar = () => {
+  const activeStyle = { color: 'blue' };
   return (
     <>
+      <Router>
       <Nav>
+
         <Bars />
   
         <NavMenu>
-          <NavLink to='/about' activeStyle>
+          <NavLink to='/' activeStyle={activeStyle}>
+            Home
+          </NavLink>
+          <NavLink to='/about' activeStyle={activeStyle}>
             About
           </NavLink>
-          <NavLink to='/production' activeStyle>
+          {/* <NavLink to='/production' activeStyle={activeStyle}>
             Events
           </NavLink>
-          <NavLink to='/annual' activeStyle>
+          <NavLink to='/annual' activeStyle={activeStyle}>
             Annual Report
           </NavLink>
-          <NavLink to='/team' activeStyle>
+          <NavLink to='/team' activeStyle={activeStyle}>
             Teams
           </NavLink>
-          <NavLink to='/blogs' activeStyle>
+          <NavLink to='/blogs' activeStyle={activeStyle}>
             Blogs
           </NavLink>
-          <NavLink to='/sign-up' activeStyle>
+          <NavLink to='/sign-up' activeStyle={activeStyle}>
             Sign Up
-          </NavLink>
-          {/* Second Nav */}
-          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+          </NavLink> */}
         </NavMenu>
-        <NavBtn>
+        {/* <NavBtn>
           <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-        </NavBtn>
+        </NavBtn> */}
       </Nav>
+
+      
+          <Routes>
+            <Route path='/' component={Home} />
+            <Route path='/about' component={Home} />
+          </Routes>
+      </Router>
     </>
   );
 };
